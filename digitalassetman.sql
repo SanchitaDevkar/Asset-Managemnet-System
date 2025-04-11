b8c5466df6ea7f9978dcf364bd72157b4f63e9d1
Sanchita Devkar <98245246+SanchitaDevkar@users.noreply.github.com>
2025-04-11 10:49:53 +0530

    Add files via upload

---
+++ digitalassetman.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 11, 2025 at 07:18 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `digitalassetman`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `asset`
+--
+-- Error reading structure for table digitalassetman.asset: #1932 - Table 'digitalassetman.asset' doesn't exist in engine
+-- Error reading data for table digitalassetman.asset: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `digitalassetman`.`asset`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `assets`
+--
+-- Error reading structure for table digitalassetman.assets: #1932 - Table 'digitalassetman.assets' doesn't exist in engine
+-- Error reading data for table digitalassetman.assets: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `digitalassetman`.`assets`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `asset_allocations`
+--
+-- Error reading structure for table digitalassetman.asset_allocations: #1932 - Table 'digitalassetman.asset_allocations' doesn't exist in engine
+-- Error reading data for table digitalassetman.asset_allocations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `digitalassetman`.`asset_allocations`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+-- Error reading structure for table digitalassetman.employee: #1932 - Table 'digitalassetman.employee' doesn't exist in engine
+-- Error reading data for table digitalassetman.employee: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `digitalassetman`.`employee`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `maintenance_records`
+--
+-- Error reading structure for table digitalassetman.maintenance_records: #1932 - Table 'digitalassetman.maintenance_records' doesn't exist in engine
+-- Error reading data for table digitalassetman.maintenance_records: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `digitalassetman`.`maintenance_records`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservations`
+--
+-- Error reading structure for table digitalassetman.reservations: #1932 - Table 'digitalassetman.reservations' doesn't exist in engine
+-- Error reading data for table digitalassetman.reservations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `digitalassetman`.`reservations`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
